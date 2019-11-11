c6ce85fd15671814d7cf58346ee125692f498a1b
Juan Carlos Longines <adramelej@gmail.com>
2019-11-11 13:48:07 -0600

    OK

---
+++ app/src/main/java/com/longines/kekaschina/MainActivity.java
@@ -0,0 +1,14 @@
+package com.longines.kekaschina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
